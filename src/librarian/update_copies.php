32fbda6073857ef90ccb154cc24ac755cfb60d1d
David-Baranec <xbaranecd@stuba.sk>
2023-11-16 14:38:14 +0100

    feat: show books, when updating copies

--- src/librarian/update_copies.php
+++ src/librarian/update_copies.php
@@ -30,6 +30,52 @@
 						
 				<input type="submit" name="b_add" value="Add Copies" />
 		</form>
+		<?php
+			$query = $con->prepare("SELECT * FROM book ORDER BY title");
+			$query->execute();
+			$result = $query->get_result();
+			if(!$result)
+				die("ERROR: Couldn't fetch books");
+			$rows = mysqli_num_rows($result);
+			if($rows == 0)
+				echo "<h2 align='center'>No books available</h2>";
+			else
+			{
+				echo "<div style='margin: 30px'>";
+				echo "<h2 align='center'>Available books</h2>";
+				echo "<div class='error-message' id='error-message'>
+						<p id='error'></p>
+					</div>";
+				echo "<table width='100%' cellpadding=10 cellspacing=10>";
+				echo "<tr>
+						<th></th>
+						<th>ISBN<hr></th>
+						<th>Title<hr></th>
+						<th>Author<hr></th>
+						<th>Category<hr></th>
+						<th>Price<hr></th>
+						<th>Copies available<hr></th>
+					</tr>";
+				for($i=0; $i<$rows; $i++)
+				{
+					$row = mysqli_fetch_array($result);
+					echo "<tr>
+							<td>
+								<label class='control control--radio'>
+									<input type='radio' name='rd_book' value=".$row[0]." />
+								<div class='control__indicator'></div>
+							</td>";
+					for($j=0; $j<6; $j++)
+						if($j == 4)
+							echo "<td>$".$row[$j]."</td>";
+						else
+							echo "<td>".$row[$j]."</td>";
+					echo "</tr>";
+				}
+				echo "</table>";
+				echo "</div>";
+			}
+			?>
 	</body>
 	
 	<?php
