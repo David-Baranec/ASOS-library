32832e11f8bb180af0f5c8457a87f83f02af246d
Tomáš Knapčok <xknapcok@stuba.sk>
2023-11-16 12:17:50 +0100

    update db connect

--- src/db_connect.php
+++ src/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect('localhost', 'root', '', 'library_db');
+	$con = mysqli_connect('127.0.0.1', 'root', '', 'library_db');
 	if(!$con)
 		die("ERROR: Couldn't connect to database");
 ?>
